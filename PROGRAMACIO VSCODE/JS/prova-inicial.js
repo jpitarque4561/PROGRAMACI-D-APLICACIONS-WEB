38a6b24a0f09ced4db66922c19fddf527d8aa801
jpitarque4561 <jpitarque4561@institutelpalau.com>
2025-10-10 18:42:43 +0200

    Update prova-inicial.js

--- PROGRAMACIO VSCODE/JS/prova-inicial.js
+++ PROGRAMACIO VSCODE/JS/prova-inicial.js
@@ -17,7 +17,7 @@ c = b + c;
 d = b + b;
 console.log(a,b,c,d);*/
 
-let a = 5;
+/*let a = 5;
 let b = 18;
 let c = 15;
 let d = 22;
@@ -25,4 +25,4 @@ a = a + 10;
 b = b + 5 - c;
 c = c + 4 + b;
 d = d + b + a;
-console.log(a,b,c,d);
+console.log(a,b,c,d);*/
