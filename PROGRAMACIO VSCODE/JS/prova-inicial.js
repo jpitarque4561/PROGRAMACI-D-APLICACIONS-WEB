45b5e47e2856684730fe03932a46abfd464ab0c4
jpitarque4561 <jpitarque4561@institutelpalau.com>
2025-10-10 18:18:41 +0200

    ok js

---
+++ PROGRAMACIO VSCODE/JS/prova-inicial.js
@@ -0,0 +1,28 @@
+/*let a = 25;
+let b = 15 + a;
+let c = b * 2;
+console.log(a,b,c);*/
+
+/*let a = 5;
+let b = 18;
+let c = 15;
+let d = 22;
+a = a + 10;
+b = b + 5 - c;
+c = c + 4 + b;
+d = d + b + a;
+a = a + 1;
+b = b + c;
+c = b + c;
+d = b + b;
+console.log(a,b,c,d);*/
+
+let a = 5;
+let b = 18;
+let c = 15;
+let d = 22;
+a = a + 10;
+b = b + 5 - c;
+c = c + 4 + b;
+d = d + b + a;
+console.log(a,b,c,d);
